05a0e48a5ad25add8464288c72fecc26ea05f314
Leighton Darkins <ljdarkins@gmail.com>
2024-08-06 12:09:53 -0500

    LD - Dataframes Work

--- python/Algorithms Book Examples/quicksort.py
+++ python/Algorithms Book Examples/quicksort.py
@@ -1,4 +1,15 @@
+
+
 def quicksort(list):
+    """
+    Sorts a list of integers using the quicksort algorithm.
+
+    Parameters:
+    list (list): The list of integers to be sorted.
+
+    Returns:
+    list: The sorted list of integers.
+    """
     if len(list) < 2:
         return list
     else:
@@ -6,5 +17,6 @@ def quicksort(list):
         less = [i for i in list[1:] if i <= pivot]
         greater = [i for i in list[1:] if i > pivot]
         return quicksort(less) + [pivot] + quicksort(greater)
-    
+
 print(quicksort([10,5,2,3]))
+
